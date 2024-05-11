62fb424e82c9ef4d2f84f4e673688ec728990062
alastergrume <kalinin.i.n@gmail.com>
2024-05-11 16:13:29 +0500

    прибрался

--- 2024_05_06_MVC_Pattern/README.txt
+++
@@ -1,3 +0,0 @@
-Команда для разархивирования файла UI
-
-:$ pyuic4.bat ..\View\MainWindow.ui -o ..\View\MainWindow.py
--- OOP/N12_MVC_Pattern/Utility/CplusDObserver.py
+++
@@ -1,8 +0,0 @@
-from abc import ABCMeta, abstractmethod
-class CplusDObserver(metaclass=ABCMeta):
-    # Абстрактный суперкласс для всех наблюдателей
-    @abstractmethod
-    def modelIsChanged(self):
-        # Метод, который будет вызван наблюдателем при изменении модели
-        pass
-

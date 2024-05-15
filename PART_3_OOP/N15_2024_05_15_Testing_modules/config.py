46153bd964a5ba78eafc8c2810901c3c5b7c1445
alastergrume <kalinin.i.n@gmail.com>
2024-05-15 20:23:05 +0500

    мыв

---
+++ PART_3_OOP/N15_2024_05_15_Testing_modules/config.py
@@ -0,0 +1,26 @@
+# Colors
+BLUE = (0, 152, 236)
+RED = (236, 0, 0)
+YELLOW = (255, 224, 22)
+ORANGE = (255, 140, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
+# COLUMS/ROWS
+ROWS = 6
+COLUMNS = 7
+
+#Disck size
+DISC_SIZE = 80
+DISC_RADIUS = int(DISC_SIZE/2 - 10)
+
+# Players
+PLAYER = 1
+AI_PLAYER = 1
+
+# Игровое поле
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PLAYER_PIECE = 2
+MAIN_WINDOW = 4

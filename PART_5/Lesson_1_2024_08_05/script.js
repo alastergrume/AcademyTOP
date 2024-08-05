7e4cd67e258b2e24c247d782ec4479a75e47037a
alastergrume <kalinin.i.n@gmail.com>
2024-08-06 01:20:38 +0500

    домашка по js

--- PART_5/Lesson_1_2024_08_05/script.js
+++ PART_5/Lesson_1_2024_08_05/script.js
@@ -33,9 +33,52 @@ ul3.append(liLast);
 
 // Домашнее задание!
 
-// 1.:
-let login_name = prompt('Введите Ваше имя ')
-alert('Привет ' +login_name +'!');
+// 1.: Приветствие пользователя по имени
+// let login_name = prompt('Введите Ваше имя ')
+// alert('Привет ' +login_name +'!');
 
-//2.:
+//2.: Вычисление возраста по году рождения
+// let birthday = prompt('Введите год Вашего рождения') // Ввод года рождения
+// let currentDate = new Date().getFullYear() // Метод выводит текущий год
+// let age = currentDate-birthday // Производит вычисления возраста
+// alert('Вам сейчас ' + age + ' лет!') // Выводит информацию о возрасте
 
+// 3.: Периметр квадрата
+// let squre_len = prompt('Введите длину стороны квадрата')
+// alert('Периметр равен - '+squre_len*2)
+
+// 4.: Вычиселение площади окружности
+// let circle_radius = prompt('Введите радиус окружности - ')
+// alert('Площадь окружности равна - ' +(circle_radius**2)*3.14)
+
+// 5.: Вычисление скорости движения между городами
+// let distance = prompt('Введите расстояние между городами')
+// let travel_time = prompt('За сколько часов необходимо добраться?')
+// alert('Скорость движения должна быть, км/ч - '+distance/travel_time)
+
+// 6.: Конвертер валют
+// let curency = prompt('Введите сумму')
+// const well = 0.91297 // Объявление константы
+// alert((curency*well).toFixed(2)+' Евро') // Умножение константы и округление вывода до двух знаков после запятой
+
+// 7.: Вычисление количества файлов размером в 820 МБ
+// let flash_value_gb = prompt('Объем Вашей флешки в Гб?')
+// const value_value_mb = 1024
+// const file_value = 820
+// let flash_mb = (flash_value_gb*value_value_mb)
+// alert('На Вашей флешке поместиться ' +Math.floor(flash_mb/file_value)+ ' файлов') // округляет в меньшую сторону
+
+// 8.: Вычисление количества шоколадок и сдачи в кошельке
+// let cash_balance = prompt('Введите остаток денежных средств')
+// let price_shokolate = prompt('Введите цену одной шоколадки')
+// let amount_chocolate = Math.floor(cash_balance/price_shokolate)
+// let end_balance = cash_balance-(amount_chocolate*price_shokolate)
+// alert('При Вашем балансе вы можете купить ' + amount_chocolate + ' шоколадок, и в кошельке останется ' + end_balance + ' рублей.')
+
+// 9.: Трехзначное число задом наперед
+let usernumber = prompt('Введите трехзначное число') //125
+let a = usernumber%100 // 1
+let b = usernumber%10 // 12.5
+let c = 
+alert('Первернутое число - '+cba)
+ 

bff93cfee4df75ba450eb5a8ccb1be199a850e3b
Ivan <kalinin.i.n@gmail.com>
2024-08-07 20:14:16 +0500

    добавлена пара

---
+++ PART_5/Lesson_2_2024_08_07/script.js
@@ -0,0 +1,72 @@
+// // Практика
+// // Задание №1
+// let value = +prompt('Inter ypur number');
+// let value = value ** 2;
+// alert("It's yuor new number - " + value);
+
+// // Задание №2
+// let value = +prompt('Inter first number');
+// let value2 = +prompt('Inter second number');
+// let value3 = (value1 + value2) / 2;
+// alert('Среднне  ' + value3);
+
+// // Задание №3
+// let value = +prompt('Inter your number');
+// let value = value * 2;
+// alert('Площадь = ', +value);
+
+// // Задание №4
+// let kilometr = +prompt('Введите в киллометрах');
+// const mili = 0.621371;
+// alert('Расстояние в милях' + (mili * kilometr));
+    
+// // Задание №5
+// value = +prompt('Введите первое число');
+// value2 = +prompt('Введите второе число');
+// let one = value + value2;
+// let two = value - value2;
+// let three = value - value2;
+// let four = value / value2;
+// alert(
+//     'Сложение' + one + 'Вычитание' + two + 'Умножение' + three + 'Деление' + four
+// );
+// Задание №6
+// let value = +prompt('Введите значение (а)');
+// let value2 = +prompt('Введите значение (b)');
+// let valuex = (value2*(-1)) / value;
+// alert('Искомое число - ' + (value/value2));
+
+//Задание №7
+// let value = +prompt('Сколько сейчас часов?');
+// let value2 = +prompt('Сколько сейчас минут?');
+// let houre = 23 - value;
+// let minute = 60 - value2;
+// alert(`До конца дня осталось ${houre} часов - ${minute} минут`)
+
+
+//Задание №8
+// let value = +prompt('Введите трехзначное число')
+// let value2 = value / 10
+// let value3 = Math.floor(value2) % 10
+// alert(value3)
+
+// альтернативный вариант, математическим путем
+// let value3 = value % 100 //Получаем 23
+// let value4 = value3 % 10 // Получаем 3
+// let value5 = (value3 - value4)/10
+// alert(value5)
+
+//Задание №9
+// let value = +prompt('Введите пятизначное число')
+// let value1 = value % 10 //5
+// let value2 = value / 10 //1234.5
+// alert(`${value1}${Math.floor(value2)}`)
+
+//Задание №10
+// let value = +prompt('Какая общая сумма продаж?');
+// const price = 250;
+// let value2 = (value * 0.1) + price;
+// alert(value2);
+
+
+// Работа с объектами
